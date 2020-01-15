1ea0e051e0dd6462895db81c6a3427d7dd45c80b
ChrisZogbi <51448389+ChrisZogbi@users.noreply.github.com>
2020-01-15 12:57:10 -0300

    Add files via upload

---
+++ controllers/UserController.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var UserService  = require('../services/UserService');
+
+ exports.getUsers = function(req, res) {
+   console.log(req.body);
+   if(req.body.UserId != null)
+   {
+      var data = UserService.getUser(req, res);
+   }
+   else
+   {
+      var data = UserService.getUsers(req, res);
+   }
+   
+   return(data);
+ };
+
+ exports.addUser = function(req, res) {
+    console.log(req.body);
+    return(UserService.addUser(req, res));
+};
+
+exports.updateUser = function(req, res) {
+  console.log(req.body);
+  return(UserService.updateUser(req, res));
+};
+
+exports.deleteUser = function(req, res) {
+  console.log(req.body);
+  return(UserService.deleteUser(req, res));
+};
+
+exports.loginUser = (req, res) => {
+  console.log("LoginUser UserController  " + req.query.Mail);
+  return(UserService.getUserByMailContrasenia(req, res))
+};
