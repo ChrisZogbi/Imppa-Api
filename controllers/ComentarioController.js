797af55277641a16404bfb44e1da623c08538f44
ChrisZogbi <chris.zogbi@gmail.com>
2020-05-17 15:17:09 -0300

    subo todo el sistema de comentarios

--- controllers/ComentarioController.js
+++ controllers/ComentarioController.js
@@ -1,40 +1,71 @@
 import express, { response } from 'express';
 import * as ComentarioService from '../services/ComentarioService';
 import { LogError } from './ErrorLogController';
+import { datacatalog } from 'googleapis/build/src/apis/datacatalog';
+
+const _ = require("lodash");
+
+const ObtenerObjetoComentario = async (Data) => {
+  return new Promise((resolve, reject) => {
+    let objetoComentario = [];
+    _.forEach(Data, (value) => {
+      objetoComentario.push({
+        IdComentario: value.IdComentario,
+        ComentarioData: { Comentario: value.Comentario, Puntaje: value.Puntaje },
+        ProfesorData: { Id: value.IdProfesor, Nombre: value.NombreProfesor, Apellido: value.ApellidoProfesor, Foto: '' },
+        AlumnoData: { Id: value.IdAlumno, Nombre: value.NombreAlumno, Foto: '' },
+        ClaseData: { Id: value.IdClaseProfesor, Categoria: value.CategoriaClase, Tipo: value.TipoClase, Foto: '' }
+      });
+    })
+    resolve(objetoComentario);
+  })
+    .then((result) => {
+      return (result)
+    });
+};
 
 export function getComentariosProfesor(req, res) {
-  const idProfesor = req.query.IdProfesor;
+  const idProfesor = req.params.IdProfesor;
+  console.log('Id profesor enviado ' + idProfesor);
   ComentarioService.getComentariosProfesor(idProfesor)
     .then((response) => {
       if (!response.Success) { return res.status(200).json(response) }
-
-      res.status(200).json({ Success: true, DataComentario: response.Data });
+      ObtenerObjetoComentario(response.Data).
+        then((result) => {
+          res.status(200).json({ Success: true, Comentarios: result });
+        });
     })
     .catch((err) => {
       LogError(getComentariosProfesor.name, `Error buscar comentarios IdProfesor: ${idProfesor}. Mensaje de error: ${err.message}`);
     });
 }
 
-export function getComentarioByClase(req, res) {
-  const idClaseProfesor = req.query.IdClaseProfesor;
+export function getComentarioClase(req, res) {
+  const idClaseProfesor = req.params.IdClaseProfesor;
   ComentarioService.getComentariosClase(idClaseProfesor)
     .then((response) => {
       if (!response.Success) { return res.status(200).json(response) }
 
-      res.status(200).json({ Success: true, DataComentario: response.Data });
+      ObtenerObjetoComentario(response.Data).
+        then((result) => {
+          res.status(200).json({ Success: true, Comentarios: result });
+        });
     })
     .catch((err) => {
-      LogError(getComentarioByClase.name, `Error buscar comentarios IdClaseProfesor: ${idClaseProfesor}. Mensaje de error: ${err.message}`);
+      LogError(getComentarioClase.name, `Error buscar comentarios IdClaseProfesor: ${idClaseProfesor}. Mensaje de error: ${err.message}`);
     });
 }
 
 export function getComentarioAlumno(req, res) {
-  const idAlumno = req.query.IdAlumno;
+  const idAlumno = req.params.IdAlumno;
   ComentarioService.getComentarioAlumno(idAlumno)
     .then((response) => {
       if (!response.Success) { return res.status(200).json(response) }
 
-      res.status(200).json({ Success: true, DataComentario: response.Data });
+      ObtenerObjetoComentario(response.Data).
+        then((result) => {
+          res.status(200).json({ Success: true, Comentarios: result });
+        });
     })
     .catch((err) => {
       LogError(getComentarioAlumno.name, `Error buscar comentarios idAlumno: ${idAlumno}. Mensaje de error: ${err.message}`);
@@ -44,7 +75,7 @@ export function getComentarioAlumno(req, res) {
 export async function addComentarioController(req, res) {
   console.log(req.body);
 
-  ComentarioService.addComentario(req)
+  ComentarioService.addComentario(req.body)
     .then((response) => {
       console.log("Respuesta" + response.Success)
 
--- services/ComentarioService.js
+++ services/ComentarioService.js
@@ -1,74 +1,107 @@
 import app from "../app.js";
 import { pool } from "./index";
 
-export function getComentariosProfesor(idProfesor)
-{
-    var query = `SELECT * FROM comentarios WHERE IDProfesor = ${idProfesor}`;
+const selectComentario =  
+                            `
+                            SELECT 
+                                c.ID as IdComentario,
+                                up.ID as IdProfesor,
+                                up.Nombre as NombreProfesor,
+                                up.Apellido as ApellidoProfesor,
+                                cp.ID as IdClaseProfesor,
+                                cc.NombreCategoria as CategoriaClase,
+                                tc.Tipo as TipoClase,
+                                ua.ID as IdAlumno,
+                                ua.Nombre as NombreAlumno,
+                                c.ID as IdComentario,
+                                c.Comentario,
+                                c.Puntaje
+                            FROM comentarios c
+                                JOIN usuarios up on c.IDProfesor = up.ID
+                                JOIN usuarios ua on c.IDAlumno = ua.ID
+                                JOIN claseprofesor cp on cp.ID = c.IDClaseProfesor 
+                                JOIN clasexusuario cu on cu.IDUsuario = c.IDProfesor and cu.IDClaseProfesor = cp.ID
+                                JOIN categoriaclase cc on cc.ID = cp.IDCategoriaClase
+                                JOIN tipoclase tc on tc.ID = cp.IDTipoClase
+                            `
 
+export function getComentariosProfesor(idProfesor) {
+    var query = ` ${selectComentario} WHERE IDProfesor = ${idProfesor}`;
+    
     return pool.promise().query(query)
-            .then(([rows]) => {return ({Success: true, Data: rows})})
-            .catch((err) => { return ({Success: false, Data: err})});
+        .then(([rows]) => { return ({ Success: true, Data: rows }) })
+        .catch((err) => { return ({ Success: false, Data: err }) });
 }
 
-export function getComentariosClase(idClaseProfesor)
-{
-    var query = `SELECT * FROM comentarios WHERE IDClaseProfesor = ${idClaseProfesor}`;
+export function getComentariosClase(idClaseProfesor) {
+    var query = ` ${selectComentario} WHERE cp.ID = ${idClaseProfesor}`;
 
     return pool.promise().query(query)
-            .then(([rows]) => {return ({Success: true, Data: rows})})
-            .catch((err) => { return ({Success: false, Data: err})});
+        .then(([rows]) => { return ({ Success: true, Data: rows }) })
+        .catch((err) => { return ({ Success: false, Data: err }) });
 }
 
-export function getComentarioAlumno(idAlumno)
-{
-    var query = `SELECT * FROM comentarios WHERE IDAlumno = ${idAlumno}`;
-
-    return pool.promise().query(query)
-            .then(([rows]) => {return ({Success: true, Data: rows})})
-            .catch((err) => { return ({Success: false, Data: err})});
-}
-
-export function getComentarioById(idComentario)
-{
-    var query = `SELECT * FROM comentarios WHERE ID = ${idComentario}`
-
-    return pool.promise().query(query)
-            .then(([rows]) => {return ({Success: true, Data: rows})})
-            .catch((err) => { return ({Success: false, Data: err})});   
-}
-
-export function addComentario(comentarioData)
-{
-    var query =  `INSERT INTO comentarios VALUES (?, ?)`
-
+export function getComentarioAlumno(idAlumno) {
+    var query = ` ${selectComentario} WHERE IDAlumno = ${idAlumno}`;
     console.log(query);
-    return pool.promise().query(query, [Data.IdProfresor, Data.Comentario, Data.Puntaje])
-            .then(() => {return ({Success: true})})
-            .catch((err) => { return ({Success: false, Data: err})});
+    return pool.promise().query(query)
+        .then(([rows]) => { return ({ Success: true, Data: rows }) })
+        .catch((err) => { return ({ Success: false, Data: err }) });
 }
 
-export function updateComentario(req, res)
-{
+export function getComentarioById(idComentario) {
+    var query = ` ${selectComentario} WHERE ID = ${idComentario}`
+
+    return pool.promise().query(query)
+        .then(([rows]) => { return ({ Success: true, Data: rows }) })
+        .catch((err) => { return ({ Success: false, Data: err }) });
+}
+
+export function addComentario(comentarioData) {
+    var query = `
+                    INSERT INTO comentarios
+                    (
+                        IDProfesor,
+                        IDAlumno,
+                        Comentario,
+                        Puntaje,
+                        IDClaseProfesor
+                    )
+                    VALUES
+                    (
+                        ${comentarioData.IDProfesor},
+                        ${comentarioData.IDAlumno},
+                        '${comentarioData.Comentario}',
+                        ${comentarioData.Puntaje},
+                        ${comentarioData.IDClaseProfesor}
+                    )
+                        `
+
+    return pool.promise().query(query)
+        .then(() => { return ({ Success: true }) })
+        .catch((err) => { return ({ Success: false, Data: err }) });
+}
+
+export function updateComentario(req, res) {
     var ComentarioData = req.body;
 
     var query = `UPDATE [comentarios]
                     SET Comentario = ${ComentarioData.Comentario}
                     ,Puntaje = '${ComentarioData.Puntaje}'
-                WHERE ID = ${ComentarioData.IdComentario}`; 
+                WHERE ID = ${ComentarioData.IdComentario}`;
     console.log(query);
     return pool.promise().query(query)
-            .then( () => {return ({Success: true})})
-            .catch((err) => { return ({Success: false, Data: err})});
+        .then(() => { return ({ Success: true }) })
+        .catch((err) => { return ({ Success: false, Data: err }) });
 }
 
-export function deleteComentario(req, res)
-{
+export function deleteComentario(req, res) {
     var idComentario = req.body.IdComentario;
 
     var query = `DELETE FROM comentarios
-                WHERE[ID = ?`; 
+                WHERE[ID = ?`;
 
     return pool.promise().query(query, [idComentario])
-            .then( () => {return ({Success: true})})
-            .catch((err) => { return ({Success: false, Data: err})});
+        .then(() => { return ({ Success: true }) })
+        .catch((err) => { return ({ Success: false, Data: err }) });
 }
